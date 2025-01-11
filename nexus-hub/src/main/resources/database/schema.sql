8f98c8ed5e3573502a2c5a8f12ca3874b0b1c39a
Agon <agon-kolgeci@hotmail.com>
2025-01-11 01:04:38 +0100

    Update schem charset

--- nexus-hub/src/main/resources/database/schema.sql
+++ nexus-hub/src/main/resources/database/schema.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS `jumps_records` (
     `player_uuid`  VARCHAR(36) NOT NULL,
     `time`  INT(11) NOT NULL,
     PRIMARY KEY (`jump_name`, `player_uuid`)
-) DEFAULT CHARSET = utf8mb4;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;;
